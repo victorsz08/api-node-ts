ae98428cb6b1269b16af15b530bffccdbd6c822b
victorsz08 <joaovictorsal777@gmail.com>
2025-06-03 14:27:19 -0300

    adjusts jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,41 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: "node",
-    testMatch: ["**/__test__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"]
-}
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'json',
+    'lcov',
+    'text',
+    'clover'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts", // Inclui todos os arquivos .ts na pasta src
+    "!src/**/*.spec.ts", // Exclui arquivos de teste da cobertura
+    "!src/**/*.test.ts"  // Garante que arquivos de teste não sejam contados como código-fonte
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  // Configuração para ts-jest
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json' // Aponta para seu arquivo tsconfig.json
+    }
+  },
+  detectOpenHandles: true,
+  passWithNoTests: true
+};
+
+export default config;
